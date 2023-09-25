b52249129a3a78d64210d77d9a7235a3b605f308
Nilesh Pant <nileshpant6969@gmail.com>
2023-09-25 18:38:50 +0530

    sdsf

--- sage_desktop_sdk/core/schema/read_only.py
+++ sage_desktop_sdk/core/schema/read_only.py
@@ -37,3 +37,38 @@ class VendorType:
     def generate_vendor_type(cls, vendor_types):
         for vendor_type_dict in vendor_types:
             yield cls(**vendor_type_dict)
+
+@dataclass
+class Commitment:
+    id: str
+    version: str
+    amount: int
+    amount_approved: float
+    amount_retained: float
+    amount_invoiced: float
+    amount_original: float
+    amount_paid: float
+    amount_pending: float
+    code: int
+    created_on_utc: str
+    date: str
+    description: str 
+    has_external_id: bool
+    is_active: bool
+    is_archived: bool
+    is_closed: bool
+    is_commited: bool
+    is_committed: bool
+    job_id: str
+    retainagePercent: float
+    name: str
+    shipToAddress1: str
+    shipToAddress2: str
+    shipToCity: str
+    shipToPostalCode: str
+    shipToState: str
+    tax: float
+    type: int
+    vendorId: str
+    was_printed: bool
+    tax_group_id: str
