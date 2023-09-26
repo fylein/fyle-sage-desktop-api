78b515a608092eeda682b98e94671dd13641cef7
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2023-09-27 00:49:31 +0530

    Vendor support with dataclasses and generator (#11)

--- sage_desktop_sdk/apis/vendors.py
+++ sage_desktop_sdk/apis/vendors.py
@@ -2,16 +2,31 @@
 Sage Desktop Vendors
 """
 from sage_desktop_sdk.core.client import Client
+from sage_desktop_sdk.core.schema.read_only import Vendor, VendorType
 
 
 class Vendors(Client):
     """Class for Accounts APIs."""
 
     GET_VENDORS = '/AccountsPayable/Api/V1/Vendor.svc/vendors'
+    GET_VENDOR_TYPES = '/AccountsPayable/Api/V1/Vendor.svc/vendors/types'
+
 
     def get_all(self):
         """
         Get all Vendors
         :return: List of Dicts in Vendors Schema
         """
-        return self._query_get_all(Vendors.GET_VENDORS)
+        vendors = self._query_get_all(Vendors.GET_VENDORS)
+        for vendor in vendors:
+            yield Vendor.from_dict(vendor)
+
+
+    def get_vendor_types(self):
+        """
+        Get Vendor Types
+        :return: List of Dicts in Vendor Types Schema
+        """
+        vendor_types = self._query_get_all(Vendors.GET_VENDOR_TYPES)
+        for vendor_type in vendor_types:
+            yield VendorType.from_dict(vendor_type)
