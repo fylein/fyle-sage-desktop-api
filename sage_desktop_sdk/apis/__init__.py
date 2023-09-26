e3bd2409620af1210a12f7c86a9902274d779873
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2023-09-27 00:53:34 +0530

    commitments support for sage 300 (#12)

--- sage_desktop_sdk/apis/__init__.py
+++ sage_desktop_sdk/apis/__init__.py
@@ -4,9 +4,11 @@ Sage Intacct SDK init
 from .accounts import Accounts
 from .vendors import Vendors
 from .jobs import Jobs
+from .commitments import Commitments
 
 __all__ = [
     'Accounts',
     'Vendors',
-    'Jobs'
+    'Jobs',
+    'Commitments'
 ]
