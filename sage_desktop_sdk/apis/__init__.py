d40b58b6c11766a34ffecb0a929fff918607c44a
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2023-09-27 00:58:30 +0530

    support for document support and post request (#7)

--- sage_desktop_sdk/apis/__init__.py
+++ sage_desktop_sdk/apis/__init__.py
@@ -5,10 +5,12 @@ from .accounts import Accounts
 from .vendors import Vendors
 from .jobs import Jobs
 from .commitments import Commitments
+from .documents import Documents
 
 __all__ = [
     'Accounts',
     'Vendors',
     'Jobs',
-    'Commitments'
+    'Commitments',
+    'Documents'
 ]
