3fed157ff5b11c399c306d30e6cb29fb3eebba4b
Nilesh Pant <nileshpant6969@gmail.com>
2023-09-27 00:47:39 +0530

    Vendor support with dataclasses and generator #4  Merged

--- sage_desktop_sdk/core/schema/read_only.py
+++ sage_desktop_sdk/core/schema/read_only.py
@@ -20,3 +20,53 @@ class Account:
             is_archived=account_dict['IsArchived'],
             name=account_dict['Name'],
         )
+
+
+@dataclass
+class Vendor:
+    id: str
+    version: int
+    code: str
+    created_on_utc: str
+    default_expense_account: str
+    default_standard_category: str
+    default_standard_costcode: str
+    has_external_id: bool
+    invoice_tax_type: int
+    is_active: bool
+    is_archived: bool
+    name: str
+    type_id: str
+
+    @classmethod
+    def from_dict(cls, vendor_dict):
+        return cls(
+            id=vendor_dict['Id'],
+            version=vendor_dict['Version'],
+            code=vendor_dict['Code'],
+            created_on_utc=vendor_dict['CreatedOnUtc'],
+            default_expense_account=vendor_dict['DefaultExpenseAccount'],
+            default_standard_costcode=vendor_dict['DefaultStandardCostCode'],
+            default_standard_category=vendor_dict['DefaultStandardCategory'],
+            has_external_id=vendor_dict['HasExternalId'],
+            invoice_tax_type=vendor_dict['InvoiceTaxType'],
+            is_active=vendor_dict['IsActive'],
+            is_archived=vendor_dict['IsArchived'],
+            name=vendor_dict['Name'],
+            type_id=vendor_dict['TypeId']
+        )
+
+
+@dataclass
+class VendorType:
+    id: str
+    version: int
+    name: str
+
+    @classmethod
+    def from_dict(cls, vendor_type):
+        return cls(
+            id=vendor_type['Id'],
+            version=vendor_type['Version'],
+            name=vendor_type['Name']
+        )
