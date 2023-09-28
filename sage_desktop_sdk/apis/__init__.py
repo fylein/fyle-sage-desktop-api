0b02db7ff80a71485e93933666e8c25de74a58cd
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2023-09-28 11:56:47 +0530

    support for operation status (#7)

--- sage_desktop_sdk/apis/__init__.py
+++ sage_desktop_sdk/apis/__init__.py
@@ -6,11 +6,13 @@ from .vendors import Vendors
 from .jobs import Jobs
 from .commitments import Commitments
 from .documents import Documents
+from .operation_status import OperationStatus
 
 __all__ = [
     'Accounts',
     'Vendors',
     'Jobs',
     'Commitments',
-    'Documents'
+    'Documents',
+    'OperationStatus'
 ]
