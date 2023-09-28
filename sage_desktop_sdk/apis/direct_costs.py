3707af7c240351e63c9bfb2027c5b659b297e5f8
Nilesh Pant <nileshpant6969@gmail.com>
2023-09-28 11:31:40 +0530

    post of direct costs

---
+++ sage_desktop_sdk/apis/direct_costs.py
@@ -0,0 +1,20 @@
+"""
+Sage Desktop Direct Cost
+"""
+import json
+from sage_desktop_sdk.core.client import Client
+from sage_desktop_sdk.core.schema.write_only import DocumentPostPayload
+
+
+class DirectCost(Client):
+    """Class for Direct Cost APIs."""
+
+    POST_DIRECT_COST= '/JobCosting/Api/V1/JobTransaction.svc/transactions/direct-costs'
+
+
+    def post_document(self, data: dict):
+        """
+        Get Vendor Types
+        :return: List of Dicts in Vendor Types Schema
+        """
+        return self._post_request(DirectCost.POST_DIRECT_COST, data=json.dumps(data.__dict__))
