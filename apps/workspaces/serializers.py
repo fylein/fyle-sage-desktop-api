023237f042803b2dc2f58274ac330249ba3015c0
Nilesh Pant <nileshpant6969@gmail.com>
2023-10-18 10:28:16 +0530

    use generic view

--- apps/workspaces/serializers.py
+++ apps/workspaces/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
+from django.conf import settings
 
 from .models import Sage300Credentials
+from sage_desktop_sdk.sage_desktop_sdk import SageDesktopSDK
+from sage_desktop_sdk.exceptions import (
+    UserAccountLocked,
+    InvalidUserCredentials,
+    InvalidWebApiClientCredentials,
+    WebApiClientLocked
+)
 
 
 class Sage300CredentialSerializer(serializers.ModelSerializer):
@@ -10,3 +18,46 @@ class Sage300CredentialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sage300Credentials
         fields = '__all__'
+
+
+class Sage300CredentialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sage300Credentials
+        fields = '__all__'
+
+
+        def create(self, validated_data):
+            try:
+                sd_username = validated_data.get('username')
+                sd_user_password = validated_data.get('password')
+                sd_identifier = validated_data.get('identifier')
+                workspace = validated_data.get('workspace')
+                sd_api_key = settings.SD_API_KEY
+                sd_api_secret = settings.SD_API_SECRET
+
+                # Initialize SageDesktopSDK or perform necessary actions.
+                SageDesktopSDK(
+                    api_key=sd_api_key,
+                    api_secret=sd_api_secret,
+                    user_name=sd_username,
+                    password=sd_user_password,
+                    indentifier=sd_identifier
+                )
+
+                # Save the Sage300Credentials instance and update the workspace
+                instance = Sage300Credentials.objects.create(
+                    username=sd_username,
+                    password=sd_user_password,
+                    identifier=sd_identifier,
+                    api_key=sd_api_key,
+                    api_secret=sd_api_secret,
+                    workspace=workspace
+                )
+
+                workspace.onboarding_state = 'EXPORT_SETTINGS'
+                workspace.save()
+
+                return instance
+
+            except (InvalidUserCredentials, InvalidWebApiClientCredentials, UserAccountLocked, WebApiClientLocked) as e:
+                raise serializers.ValidationError(str(e))
--- apps/workspaces/urls.py
+++ apps/workspaces/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import ConnectSage300View
+from apps.workspaces.views import Sage300CredsView
 
 
 urlpatterns = [
-    path('<int:workspace_id>/credentials/sage_300/delete/', ConnectSage300View.as_view({'post': 'delete'}), name='sage300-delete'),
-    path('<int:workspace_id>/credentials/sage_300/', ConnectSage300View.as_view({'post': 'post', 'get': 'get'}), name='sage300-creds')
+    path('<int:workspace_id>/export_settings/', Sage300CredsView.as_view(), name='sage300-creds'),
 ]
