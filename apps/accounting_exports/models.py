2bfcbf05aa3798f395b7e3e83eee74fe38a1b129
ruuushhh <rushikeshtayade2001@gmail.com>
2023-10-23 11:11:17 +0530

    Accounting export apis added

--- apps/accounting_exports/models.py
+++ apps/accounting_exports/models.py
@@ -23,7 +23,7 @@ class AccountingExport(BaseModel):
     expenses = models.ManyToManyField(Expense, help_text="Expenses under this Expense Group")
     task_id = StringNullField(help_text='Fyle Jobs task reference')
     description = CustomJsonField(help_text='Description')
-    invoiced = models.ForeignKey(Invoice, on_delete=models.PROTECT, help_text='Referenc to Invoie', null=True)
+    invoice = models.ForeignKey(Invoice, on_delete=models.PROTECT, help_text='Referenc to Invoie', null=True)
     direct_cost = models.ForeignKey(DirectCost, on_delete=models.PROTECT, help_text='Reference to DirectCost', null=True)
     status = StringNotNullField(help_text='Task Status')
     detail = CustomJsonField(help_text='Task Response')
