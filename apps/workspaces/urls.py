0edb98d6a29378d6704549dc460d490cb9fdce4e
ruuushhh <rushikeshtayade2001@gmail.com>
2023-10-23 11:12:52 +0530

    Accounting export apis added

--- apps/workspaces/urls.py
+++ apps/workspaces/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('<int:workspace_id>/advanced_settings/', AdvancedSettingView.as_view(), name='advanced-settings'),
     path('<int:workspace_id>/admins/', WorkspaceAdminsView.as_view(), name='admin'),
     path('<int:workspace_id>/fyle/', include('apps.fyle.urls')),
+    path('<int:workspace_id>/accounting_exports/', include('apps.accounting_exports.urls')),
 ]
