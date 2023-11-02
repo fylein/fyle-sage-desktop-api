2ad1ef1b69eeeb4106461ffa3d672e71db7479d5
ruuushhh <rushikeshtayade2001@gmail.com>
2023-11-02 14:10:29 +0530

    Test cases added

---
+++ tests/test_mappings/test_imports/test_modules/fixtures.py
@@ -0,0 +1,33 @@
+data = {
+    "create_fyle_project_payload_create_new_case":
+    [
+        {
+            'name':'Direct Mail Campaign',
+            'code':'10064',
+            'description':'Sage 300 Project - Direct Mail Campaign, Id - 10064',
+            'is_enabled':True
+        },
+        {
+            'name':'Platform APIs',
+            'code':'10081',
+            'description':'Sage 300 Project - Platform APIs, Id - 10081',
+            'is_enabled':True
+        }
+
+    ],
+    "create_fyle_project_payload_create_disable_case":
+    [
+        {
+            'code': '10064',
+            'description': 'Sage 300 Project - Direct Mail Campaign, Id - 10064',
+            'is_enabled': True,
+            'name': 'Direct Mail Campaign'
+        },
+        {
+            'code': '10081',
+            'description': 'Sage 300 Project - Platform APIs, Id - 10081',
+            'is_enabled': False,
+            'name': 'Platform APIs'
+        }
+    ]
+}
