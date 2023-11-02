3a67d731bbf96ee7362efe06802a91fb6aaf4339
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2023-11-02 14:27:14 +0530

    PR 4: post dependent field to fyle (#61)

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -2,8 +2,14 @@ import logging
 from rest_framework import generics
 from sage_desktop_api.utils import LookupFieldMixin
 from apps.workspaces.models import Workspace
-from apps.fyle.serializers import ImportFyleAttributesSerializer, ExpenseFilterSerializer, ExpenseFieldSerializer, FyleFieldsSerializer
-from apps.fyle.models import ExpenseFilter
+from apps.fyle.serializers import (
+    ImportFyleAttributesSerializer,
+    ExpenseFilterSerializer,
+    ExpenseFieldSerializer,
+    FyleFieldsSerializer,
+    DependentFieldSettingSerializer
+)
+from apps.fyle.models import ExpenseFilter, DependentFieldSetting
 
 logger = logging.getLogger(__name__)
 logger.level = logging.INFO
@@ -52,3 +58,14 @@ class FyleFieldsView(generics.ListAPIView):
 
     def get_queryset(self):
         return FyleFieldsSerializer().format_fyle_fields(self.kwargs["workspace_id"])
+
+
+class DependentFieldSettingView(generics.CreateAPIView, generics.RetrieveUpdateAPIView):
+    """
+    Dependent Field view
+    """
+    authentication_classes = []
+    permission_classes = []
+    serializer_class = DependentFieldSettingSerializer
+    lookup_field = 'workspace_id'
+    queryset = DependentFieldSetting.objects.all()
