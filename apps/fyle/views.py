ae3d2f9362b38f4c2ad66cbc5bec3873a46f3882
Nilesh Pant <nileshpant6969@gmail.com>
2023-11-07 16:57:23 +0530

    fix

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -14,7 +14,7 @@ from apps.fyle.serializers import (
 )
 from apps.fyle.models import ExpenseFilter, DependentFieldSetting
 from apps.fyle.helpers import get_exportable_accounting_exports_ids
-from apps.fyle.queue import queue_import_reimbursable_expenses
+from apps.fyle.queue import queue_import_reimbursable_expenses, queue_import_credit_card_expenses
 
 
 logger = logging.getLogger(__name__)
@@ -99,6 +99,7 @@ class AccoutingExportSyncView(generics.CreateAPIView):
         """
 
         queue_import_reimbursable_expenses(kwargs['workspace_id'], synchronous=True)
+        queue_import_credit_card_expenses(kwargs['workspace_id'], synchronous=True)
 
         return Response(
             status=status.HTTP_200_OK
