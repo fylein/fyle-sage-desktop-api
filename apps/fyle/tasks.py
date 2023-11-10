3d7d112371cf1391a6b0af8a09c6d1af25011dfd
Nilesh Pant <nileshpant6969@gmail.com>
2023-11-10 15:08:57 +0530

    comments resolved

--- apps/fyle/tasks.py
+++ apps/fyle/tasks.py
@@ -5,17 +5,15 @@ All Tasks from which involve Fyle APIs
 2. Import Credit Card Expenses from Fyle
 """
 import logging
-import traceback
 from datetime import datetime
 from django.db import transaction
 
 from fyle_integrations_platform_connector import PlatformConnector
-from fyle.platform.exceptions import NoPrivilegeError
 
 from apps.accounting_exports.models import AccountingExport
 from apps.workspaces.models import ExportSetting, Workspace, FyleCredential
 from apps.fyle.models import Expense
-
+from apps.fyle.exceptions import handle_exceptions
 
 SOURCE_ACCOUNT_MAP = {
     'PERSONAL': 'PERSONAL_CASH_ACCOUNT',
@@ -26,134 +24,68 @@ logger = logging.getLogger(__name__)
 logger.level = logging.INFO
 
 
+def import_expenses(workspace_id, accounting_export: AccountingExport, source_account_type, fund_source_key):
+    """
+    Common logic for importing expenses from Fyle
+    :param accounting_export: Task log object
+    :param workspace_id: workspace id
+    :param source_account_type: Fyle source account type
+    :param fund_source_key: Key for accessing fund source specific fields in ExportSetting
+    """
+
+    fund_source_map = {
+        'PERSONAL': 'reimbursable',
+        'CCC': 'credit_card'
+    }
+    export_settings = ExportSetting.objects.get(workspace_id=workspace_id)
+    workspace = Workspace.objects.get(pk=workspace_id)
+    last_synced_at = getattr(workspace, f"{fund_source_key.lower()}_last_synced_at", None)
+    fyle_credentials = FyleCredential.objects.get(workspace_id=workspace_id)
+
+    platform = PlatformConnector(fyle_credentials)
+
+    expenses = platform.expenses.get(
+        source_account_type=[source_account_type],
+        state=getattr(export_settings, f"{fund_source_key.lower()}_expense_state"),
+        settled_at=last_synced_at if getattr(export_settings, f"{fund_source_map.get(fund_source_key)}_expense_state") == 'PAYMENT_PROCESSING' else None,
+        approved_at=last_synced_at if getattr(export_settings, f"{fund_source_map.get(fund_source_key)}_expense_state") == 'APPROVED' else None,
+        filter_credit_expenses=(fund_source_key == 'CCC'),
+        last_paid_at=last_synced_at if getattr(export_settings, f"{fund_source_key.lower()}_expense_state") == 'PAID' else None
+    )
+
+    if expenses:
+        setattr(workspace, f"{fund_source_map.get(fund_source_key)}_last_synced_at", datetime.now())
+        workspace.save()
+
+    with transaction.atomic():
+        expenses_object = Expense.create_expense_objects(expenses, workspace_id)
+        AccountingExport.create_accounting_export_report_id(
+            expenses_object,
+            fund_source=fund_source_key,
+            workspace_id=workspace_id
+        )
+
+    accounting_export.status = 'COMPLETE'
+    accounting_export.errors = None
+
+    accounting_export.save()
+
+
+@handle_exceptions
 def import_reimbursable_expenses(workspace_id, accounting_export: AccountingExport):
     """
     Import reimbursable expenses from Fyle
-    :param accounting_export: Task log object
+    :param accounting_export: Accounting Export object
     :param workspace_id: workspace id
     """
-    try:
-        # Get export settings to determine Expense State
-        export_settings = ExportSetting.objects.get(workspace_id=workspace_id)
-        workspace = Workspace.objects.get(pk=workspace_id)
-        last_synced_at = workspace.last_synced_at
-        fyle_credentials = FyleCredential.objects.get(workspace_id=workspace_id)
-
-        platform = PlatformConnector(fyle_credentials)
-
-        expenses = platform.expenses.get(
-            source_account_type=['PERSONAL_CASH_ACCOUNT'],
-            state=export_settings.reimbursable_expense_state,
-            settled_at=last_synced_at if export_settings.reimbursable_expense_state == 'PAYMENT_PROCESSING' else None,
-            filter_credit_expenses=True,
-            last_paid_at=last_synced_at if export_settings.reimbursable_expense_state == 'PAID' else None
-        )
-
-        if expenses:
-            workspace.last_synced_at = datetime.now()
-            workspace.save()
-
-        with transaction.atomic():
-            expenses_object = Expense.create_expense_objects(expenses, workspace_id)
-            AccountingExport.create_accounting_export_report_id(
-                expenses_object,
-                fund_source='PERSONAL',
-                workspace_id=workspace_id
-            )
-
-        accounting_export.status = 'COMPLETE'
-        accounting_export.detail = None
-
-        accounting_export.save()
-
-    except FyleCredential.DoesNotExist:
-        logger.info('Fyle credentials not found %s', workspace_id)
-        accounting_export.detail = {
-            'message': 'Fyle credentials do not exist in workspace'
-        }
-        accounting_export.status = 'FAILED'
-        accounting_export.save()
-
-    except NoPrivilegeError:
-        logger.info('Invalid Fyle Credentials / Admin is disabled')
-        accounting_export.detail = {
-            'message': 'Invalid Fyle Credentials / Admin is disabled'
-        }
-        accounting_export.status = 'FAILED'
-        accounting_export.save()
-
-    except Exception:
-        error = traceback.format_exc()
-        accounting_export.detail = {
-            'error': error
-        }
-        accounting_export.status = 'FATAL'
-        accounting_export.save()
-        logger.exception('Something unexpected happened workspace_id: %s %s', accounting_export.workspace_id, accounting_export.detail)
+    import_expenses(workspace_id, accounting_export, 'PERSONAL_CASH_ACCOUNT', 'PERSONAL')
 
 
+@handle_exceptions
 def import_credit_card_expenses(workspace_id, accounting_export: AccountingExport):
     """
     Import credit card expenses from Fyle
-    :param accounting_export: Task log object
+    :param accounting_export: AccountingExport object
     :param workspace_id: workspace id
     """
-    try:
-        # Get export settings to determine Expense State
-        export_settings = ExportSetting.objects.get(workspace_id=workspace_id)
-        workspace = Workspace.objects.get(pk=workspace_id)
-        last_synced_at = workspace.ccc_last_synced_at
-        fyle_credentials = FyleCredential.objects.get(workspace_id=workspace_id)
-
-        platform = PlatformConnector(fyle_credentials)
-
-        expenses = platform.expenses.get(
-            source_account_type=['PERSONAL_CORPORATE_CREDIT_CARD_ACCOUNT'],
-            state=export_settings.credit_card_expense_state,
-            settled_at=last_synced_at if export_settings.credit_card_expense_state == 'PAYMENT_PROCESSING' else None,
-            approved_at=last_synced_at if export_settings.credit_card_expense_state == 'APPROVED' else None,
-            filter_credit_expenses=False,
-            last_paid_at=last_synced_at if export_settings.credit_card_expense_state == 'PAID' else None
-        )
-
-        if expenses:
-            workspace.ccc_last_synced_at = datetime.now()
-            workspace.save()
-
-        with transaction.atomic():
-            expenses_object = Expense.create_expense_objects(expenses, workspace_id)
-            AccountingExport.create_accounting_export_report_id(
-                expenses_object,
-                fund_source='CCC',
-                workspace_id=workspace_id
-            )
-
-        accounting_export.status = 'COMPLETE'
-        accounting_export.errors = None
-
-        accounting_export.save()
-
-    except FyleCredential.DoesNotExist:
-        logger.info('Fyle credentials not found %s', workspace_id)
-        accounting_export.errors = {
-            'message': 'Fyle credentials do not exist in workspace'
-        }
-        accounting_export.status = 'FAILED'
-        accounting_export.save()
-
-    except NoPrivilegeError:
-        logger.info('Invalid Fyle Credentials / Admin is disabled')
-        accounting_export.detail = {
-            'message': 'Invalid Fyle Credentials / Admin is disabled'
-        }
-        accounting_export.status = 'FAILED'
-        accounting_export.save()
-
-    except Exception:
-        error = traceback.format_exc()
-        accounting_export.errors = {
-            'error': error
-        }
-        accounting_export.status = 'FATAL'
-        accounting_export.save()
-        logger.exception('Something unexpected happened workspace_id: %s %s', accounting_export.workspace_id, accounting_export.errors)
+    import_expenses(workspace_id, accounting_export, 'PERSONAL_CORPORATE_CREDIT_CARD_ACCOUNT', 'CCC')
