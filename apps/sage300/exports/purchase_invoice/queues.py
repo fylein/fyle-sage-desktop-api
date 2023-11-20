f1e2023dccc3a3d3678f133e841efd79f1c3aa78
Nilesh Pant <nileshpant6969@gmail.com>
2023-11-20 14:10:24 +0530

    naming fixed

--- apps/sage300/exports/purchase_invoice/queues.py
+++ apps/sage300/exports/purchase_invoice/queues.py
@@ -38,7 +38,7 @@ def check_accounting_export_and_start_import(workspace_id: int, accounting_expor
         Todo: Add last export details
         """
 
-        chain.append('apps.sage300.exports.purchase_invoice.helpers.create_purchase_invoice', accounting_export)
+        chain.append('apps.sage300.exports.purchase_invoice.tasks.create_purchase_invoice', accounting_export)
 
         if chain.length() > 1:
             chain.run()
--- apps/sage300/exports/purchase_invoice/tasks.py
+++ apps/sage300/exports/purchase_invoice/tasks.py
@@ -1,34 +1,53 @@
-from apps.sage300.exports.tasks import AccountingDataExporter
+from apps.sage300.exports.accounting_export import AccountingDataExporter
+from apps.accounting_exports.models import AccountingExport
 from apps.sage300.utils import SageDesktopConnector
+from apps.sage300.exports.purchase_invoice.queues import check_accounting_export_and_start_import
 
 
 class ExportPurchaseInvoice(AccountingDataExporter):
-
     """
-    Class for purchase invoice module
+    Class for handling the export of purchase invoices to Sage 300.
+    Extends the base AccountingDataExporter class.
     """
 
     def trigger_export(self, workspace_id, accounting_export_ids):
         """
-        Trigger import for Project module
+        Trigger the import process for the Project module.
         """
-        from apps.sage300.exports.purchase_invoice.queues import check_accounting_export_and_start_import
         check_accounting_export_and_start_import(workspace_id, accounting_export_ids)
 
-    def __construct_purchase_invoice(item, lineitem):
+    def __construct_purchase_invoice(self, item, lineitem):
+        """
+        Construct the payload for the purchase invoice.
+        """
+        # Implementation for constructing the purchase invoice payload goes here
         pass
 
     def post(self, item, lineitem):
         """
-        Export Purchase Invoice
+        Export the purchase invoice to Sage 300.
         """
-
         try:
             purchase_invoice_payload = self.__construct_purchase_invoice(item, lineitem)
 
+            # Establish a connection to Sage 300
             sage300_connection = SageDesktopConnector()
+
+            # Post the purchase invoice to Sage 300
             created_purchase_invoice = sage300_connection.connection.documents.post_document(purchase_invoice_payload)
             return created_purchase_invoice
 
         except Exception as e:
             print(e)
+
+
+def create_purchase_invoice(accounting_export: AccountingExport):
+    """
+    Helper function to create and export a purchase invoice.
+    """
+    export_purchase_invoice_instance = ExportPurchaseInvoice()
+
+    # Create and export the purchase invoice using the base class method
+    exported_purchase_invoice = export_purchase_invoice_instance.create_sage300_object(accounting_export=accounting_export)
+
+    return exported_purchase_invoice
