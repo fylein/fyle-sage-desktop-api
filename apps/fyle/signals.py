a75b507c2b2ae576bbcca800a9cf5f1406a081b3
Nilesh Pant <nileshpant6969@gmail.com>
2024-01-09 18:08:43 +0530

    lint fix

--- apps/fyle/signals.py
+++ apps/fyle/signals.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from fyle_integrations_platform_connector import PlatformConnector
-from apps.workspaces.models import FyleCredential, ImportSetting
+from apps.workspaces.models import FyleCredential
 from apps.fyle.models import DependentFieldSetting
 from apps.sage300.dependent_fields import create_dependent_custom_field_in_fyle
 from apps.mappings.imports.schedules import schedule_or_delete_dependent_field_tasks
