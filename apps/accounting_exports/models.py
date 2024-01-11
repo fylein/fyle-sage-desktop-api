8985eb6cee57cc42b9e11933d56dc830dcbc75a5
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2024-01-11 21:15:31 +0530

    minor bug fixes and resolve employee mapping error (#126)

--- apps/accounting_exports/models.py
+++ apps/accounting_exports/models.py
@@ -129,7 +129,7 @@ class AccountingExport(BaseForeignWorkspaceModel):
             # Calculate and assign 'last_spent_at' based on the chosen date field
             if date_field == 'last_spent_at':
                 latest_expense = Expense.objects.filter(id__in=accounting_export['expense_ids']).order_by('-spent_at').first()
-                accounting_export['last_spent_at'] = latest_expense.spent_at if latest_expense else None
+                accounting_export['last_spent_at'] = latest_expense.spent_at.strftime('%Y-%m-%d') if latest_expense else None
 
             # Store expense IDs and remove unnecessary keys
             expense_ids = accounting_export['expense_ids']
