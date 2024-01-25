ad46c51799d671152a46e419a8a38820682ee24f
ruuushhh <rushikeshtayade2001@gmail.com>
2024-01-25 16:18:37 +0530

    Sentry added

---
+++ sage_desktop_api/sentry.py
@@ -0,0 +1,40 @@
+import os
+
+import gevent
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+class Sentry:
+    @staticmethod
+    def init():
+        sentry_sdk.init(
+            dsn=os.environ.get('SENTRY_DSN'),
+            send_default_pii=True,
+            integrations=[DjangoIntegration()],
+            environment=os.environ.get('SENTRY_ENV'),
+            traces_sampler=Sentry.traces_sampler,
+            attach_stacktrace=True,
+            before_send=Sentry.before_send,
+            request_bodies='small',
+            in_app_include=['apps.users', 'apps.workspaces', 'apps.mappings', 'apps.fyle', 'apps.sage300', 'apps.accounting_exports', 'fyle_rest_auth', 'fyle_accounting_mappings'],
+        )
+
+    @staticmethod
+    def traces_sampler(sampling_context):
+        # avoiding ready APIs in performance tracing
+        if sampling_context.get('wsgi_environ') is not None:
+            if 'ready/' in sampling_context['wsgi_environ']['PATH_INFO']:
+                return 0
+
+        return 1
+
+    @staticmethod
+    def before_send(event, hint):
+        if 'exc_info' in hint:
+            exc_type, exc_value, tb = hint['exc_info']
+            if isinstance(exc_value, (gevent.GreenletExit)):
+                return None
+            elif exc_value.args[0] in ['Error: 502']:
+                return None
+        return event
