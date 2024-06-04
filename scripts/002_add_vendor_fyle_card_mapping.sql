d23f0cd8fe70219054446b488376ab36582efd6a
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-06-04 21:28:05 +0530

    modify post-release script

--- scripts/002_add_vendor_fyle_card_mapping.sql
+++ scripts/002_add_vendor_fyle_card_mapping.sql
@@ -1,10 +1,6 @@
 rollback;
 begin;
 
-select COUNT(*) from export_settings where credit_card_expense_export_type = 'PURCHASE_INVOICE';
-
-select COUNT(distinct(workspace_id)) from mapping_settings where workspace_id  not in (select distinct(workspace_id) from mapping_settings where source_field = 'CORPORATE_CARD' and destination_field = 'VENDOR');
-
 insert into mapping_settings (source_field, destination_field, import_to_fyle, is_custom, workspace_id, created_at, updated_at)
 select 'CORPORATE_CARD', 'VENDOR', 'f', 'f', ws.id, now(), now()
 from workspaces ws
@@ -18,3 +14,7 @@ where not exists (
     and ms.workspace_id = ws.id
     and es.credit_card_expense_export_type = 'PURCHASE_INVOICE'
 );
+
+select COUNT(*) from export_settings where credit_card_expense_export_type = 'PURCHASE_INVOICE';
+
+select COUNT(distinct(workspace_id)) from mapping_settings where workspace_id  not in (select distinct(workspace_id) from mapping_settings where source_field = 'CORPORATE_CARD' and destination_field = 'VENDOR');
