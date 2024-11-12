a380b9f67473b900add91cafe99d4705ccb3b6d6
Viswas Haridas <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2024-11-12 14:01:52 +0530

    fix: remove sentry transaction sampling (#227)

--- sage_desktop_api/sentry.py
+++ sage_desktop_api/sentry.py
@@ -13,21 +13,11 @@ class Sentry:
             send_default_pii=True,
             integrations=[DjangoIntegration()],
             environment=os.environ.get('SENTRY_ENV'),
-            traces_sampler=Sentry.traces_sampler,
             attach_stacktrace=True,
             before_send=Sentry.before_send,
             request_bodies='small',
             in_app_include=['apps.users', 'apps.workspaces', 'apps.mappings', 'apps.fyle', 'apps.sage300', 'apps.accounting_exports', 'fyle_rest_auth', 'fyle_accounting_mappings'],
         )
-
-    @staticmethod
-    def traces_sampler(sampling_context):
-        # avoiding ready APIs in performance tracing
-        if sampling_context.get('wsgi_environ') is not None:
-            if 'ready/' in sampling_context['wsgi_environ']['PATH_INFO']:
-                return 0
-
-        return 1
 
     @staticmethod
     def before_send(event, hint):
