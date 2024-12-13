c8b178faf15373210e16b7942994c33e86b11c29
Viswas Haridas <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2024-12-14 00:58:49 +0530

    fix: raise notice to delete dependent fields after workspace reset (#230)
    * fix: raise notice to delete dependent fields after workspace reset
    
    * refactor: squish query into one-liner
    
    * fix: dependent field queries not printing the org id (#231)
    
    * fix: dependent field queries not printing the org id
    
    * fix: delete `expense_attributes_deletion_cache` while workspace reset (#232)

--- scripts/sql/reset_db.sql
+++ scripts/sql/reset_db.sql
@@ -196,10 +196,23 @@ BEGIN
   RAISE NOTICE 'Deleted % users', rcount;
 
   DELETE
+  FROM expense_attributes_deletion_cache
+  WHERE workspace_id = _workspace_id;
+  GET DIAGNOSTICS rcount = ROW_COUNT;
+  RAISE NOTICE 'Deleted % expense_attributes_deletion_cache', rcount;
+
+
+  _org_id := (SELECT org_id FROM workspaces WHERE id = _workspace_id);
+
+  DELETE
   FROM workspaces w
   WHERE w.id = _workspace_id;
   GET DIAGNOSTICS rcount = ROW_COUNT;
   RAISE NOTICE 'Deleted % workspaces', rcount;
+
+  RAISE NOTICE E'\n\n\n\n\n\n\n\n\nSwitch to prod db and run the below queries to delete dependent fields';
+  RAISE NOTICE E'rollback;begin; delete from platform_schema.dependent_expense_field_mappings where expense_field_id in (select id from platform_schema.expense_fields where org_id =''%'' and type=''DEPENDENT_SELECT''); delete from platform_schema.expense_fields where org_id = ''%'' and type = ''DEPENDENT_SELECT'';\n\n\n\n\n\n\n\n\n\n\n', _org_id, _org_id;
+
 RETURN;
 END
 $$ LANGUAGE plpgsql;
