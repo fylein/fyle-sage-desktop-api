7eed3682a9302d39b5858bcaaa654ab2992eb14b
Rushi <rushi@Rushis-MacBook-Air.local>
2025-02-21 14:41:19 +0530

    fix run_import_export

--- apps/sage300/actions.py
+++ apps/sage300/actions.py
@@ -5,26 +5,20 @@ from apps.accounting_exports.models import AccountingExport, AccountingExportSum
 
 def update_accounting_export_summary(workspace_id):
     accounting_export_summary = AccountingExportSummary.objects.get(workspace_id=workspace_id)
-    print(accounting_export_summary.__dict__)
 
     failed_exports = AccountingExport.objects.filter(~Q(type__in=['FETCHING_REIMBURSABLE_EXPENSES', 'FETCHING_CREDIT_CARD_EXPENSES']), workspace_id=workspace_id, status__in=['FAILED', 'FATAL']).count()
     filters = {
         'workspace_id': workspace_id,
         'status': 'COMPLETE'
     }
-    print(filters, "1")
 
     if accounting_export_summary:
         filters['updated_at__gte'] = accounting_export_summary.last_exported_at
-
-    print(filters, "2")
 
     successful_exports = AccountingExport.objects.filter(
         ~Q(type__in=['FETCHING_REIMBURSABLE_EXPENSES', 'FETCHING_CREDIT_CARD_EXPENSES']),
         **filters
     ).count()
-
-    print(successful_exports.__dict__, "3")
 
     accounting_export_summary.failed_accounting_export_count = failed_exports
     accounting_export_summary.successful_accounting_export_count = successful_exports
--- apps/workspaces/tasks.py
+++ apps/workspaces/tasks.py
@@ -31,8 +31,8 @@ def run_import_export(workspace_id: int, export_mode = None):
 
     export_settings = ExportSetting.objects.get(workspace_id=workspace_id)
     advance_settings = AdvancedSetting.objects.get(workspace_id=workspace_id)
-    accounting_export = AccountingExport.objects.filter(workspace_id=workspace_id).first()
-    if not accounting_export:
+    accounting_summary = AccountingExportSummary.objects.filter(workspace_id=workspace_id).first()
+    if not accounting_summary:
         accounting_summary, _ = AccountingExportSummary.objects.update_or_create(
             workspace_id=workspace_id
         )
