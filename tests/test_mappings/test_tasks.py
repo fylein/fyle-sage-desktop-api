6275e697fdbfa5cef180d853d4a8a47a57be0e01
Hrishabh Tiwari <hrishabh.t@fyle.in>
2025-04-16 13:23:16 +0530

    add tests

--- tests/test_mappings/test_tasks.py
+++ tests/test_mappings/test_tasks.py
@@ -1,4 +1,6 @@
-from apps.mappings.tasks import sync_sage300_attributes
+from fyle_accounting_mappings.models import MappingSetting
+from fyle_integrations_imports.models import ImportLog
+from apps.mappings.tasks import sync_dependent_fields, sync_sage300_attributes, construct_tasks_and_chain_import_fields_to_fyle
 
 
 def test_sync_sage300_attributes(
@@ -33,3 +35,38 @@ def test_sync_sage300_attributes(
         assert getattr(mock_sage_connection.return_value, f'sync_{attribute_call}').call_count == 1
 
     assert mock_sage_connection.call_count == len(attribute_types)
+
+
+def test_sync_dependent_fields(db, mocker, create_temp_workspace, add_sage300_creds):
+    """
+    Test sync dependent fields
+    """
+    workspace_id = 1
+    mocked_sync_fn = mocker.patch(
+        'apps.mappings.tasks.sync_sage300_attributes'
+    )
+
+    sync_dependent_fields(workspace_id=workspace_id)
+    assert mocked_sync_fn.call_count == 3
+
+
+def test_construct_tasks_and_chain_import_fields_to_fyle(db, mocker, create_temp_workspace, add_import_settings, add_sage300_creds, add_dependent_field_setting):
+    """
+    Test construct tasks and chain import fields to fyle
+    """
+    workspace_id = 1
+    MappingSetting.objects.create(
+        source_field='PROJECT',
+        destination_field='JOB',
+        workspace_id=workspace_id,
+        import_to_fyle=True,
+        is_custom=False
+    )
+
+    mock_chain_fn = mocker.patch(
+        'apps.mappings.tasks.chain_import_fields_to_fyle'
+    )
+
+    construct_tasks_and_chain_import_fields_to_fyle(workspace_id=workspace_id)
+
+    assert mock_chain_fn.call_count == 1
