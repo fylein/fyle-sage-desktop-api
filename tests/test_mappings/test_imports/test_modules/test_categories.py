c2b56a0777b93c5b34fc2224dcd654987c5fd5a3
Hrishabh Tiwari <hrishabh.t@fyle.in>
2025-04-16 13:05:10 +0530

    fix tests

--- tests/test_mappings/test_imports/test_modules/test_categories.py
+++ tests/test_mappings/test_imports/test_modules/test_categories.py
@@ -1,227 +1,229 @@
-# from apps.workspaces.models import ImportSetting
-# from apps.mappings.imports.modules.categories import Category, disable_categories
-# from fyle_accounting_mappings.models import CategoryMapping, DestinationAttribute, ExpenseAttribute
-# from tests.test_mappings.test_imports.test_modules.fixtures import data as destination_attributes_data
-# from .fixtures import data
+from apps.workspaces.models import ImportSetting
+from fyle_accounting_mappings.models import CategoryMapping, DestinationAttribute, ExpenseAttribute
+from fyle_integrations_imports.modules.categories import Category, disable_categories
+from tests.test_mappings.test_imports.test_modules.fixtures import data as destination_attributes_data
+from .fixtures import data
 
 
-# def test_construct_fyle_payload(
-#     api_client,
-#     test_connection,
-#     create_temp_workspace,
-#     add_fyle_credentials,
-#     add_sage300_creds,
-#     add_expense_destination_attributes_1,
-#     mocker,
-# ):
-#     category = Category(1, "ACCOUNT", None)
+def test_construct_fyle_payload(
+    api_client,
+    mocker,
+    test_connection,
+    create_temp_workspace,
+    add_fyle_credentials,
+    add_sage300_creds,
+    add_expense_destination_attributes_1,
+):
+    category = Category(1, "ACCOUNT", None, sdk_connection=mocker.Mock(), destination_sync_methods=['accounts'], is_auto_sync_enabled=True, is_3d_mapping=False, charts_of_accounts=None)
 
-#     # create new case
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(
-#         workspace_id=1, attribute_type="ACCOUNT"
-#     )
-#     existing_fyle_attributes_map = {}
-#     is_auto_sync_status_allowed = category.get_auto_sync_permission()
+    # create new case
+    paginated_destination_attributes = DestinationAttribute.objects.filter(
+        workspace_id=1, attribute_type="ACCOUNT"
+    )
+    existing_fyle_attributes_map = {}
 
-#     fyle_payload = category.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         is_auto_sync_status_allowed,
-#     )
+    fyle_payload = category.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map,
+    )
 
-#     assert (
-#         fyle_payload
-#         == destination_attributes_data["create_fyle_category_payload_create_new_case"]
-#     )
+    assert (
+        fyle_payload
+        == destination_attributes_data["create_fyle_category_payload_create_new_case"]
+    )
 
-#     # disable case
-#     DestinationAttribute.objects.filter(
-#         workspace_id=1, attribute_type="ACCOUNT", value__in=["Internet", "Meals"]
-#     ).update(active=False)
+    # disable case
+    DestinationAttribute.objects.filter(
+        workspace_id=1, attribute_type="ACCOUNT", value__in=["Internet", "Meals"]
+    ).update(active=False)
 
-#     ExpenseAttribute.objects.filter(
-#         workspace_id=1, attribute_type="ACCOUNT", value__in=["Internet", "Meals"]
-#     ).update(active=True)
+    ExpenseAttribute.objects.filter(
+        workspace_id=1, attribute_type="ACCOUNT", value__in=["Internet", "Meals"]
+    ).update(active=True)
 
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(
-#         workspace_id=1, attribute_type="ACCOUNT"
-#     )
+    paginated_destination_attributes = DestinationAttribute.objects.filter(
+        workspace_id=1, attribute_type="ACCOUNT"
+    )
 
-#     paginated_destination_attribute_values = [
-#         attribute.value for attribute in paginated_destination_attributes
-#     ]
-#     existing_fyle_attributes_map = category.get_existing_fyle_attributes(
-#         paginated_destination_attribute_values
-#     )
+    paginated_destination_attribute_values = [
+        attribute.value for attribute in paginated_destination_attributes
+    ]
+    existing_fyle_attributes_map = category.get_existing_fyle_attributes(
+        paginated_destination_attribute_values
+    )
 
-#     fyle_payload = category.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         is_auto_sync_status_allowed,
-#     )
+    fyle_payload = category.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map,
+    )
 
-#     assert (
-#         fyle_payload
-#         == destination_attributes_data[
-#             "create_fyle_category_payload_create_disable_case"
-#         ]
-#     )
+    assert (
+        fyle_payload
+        == destination_attributes_data[
+            "create_fyle_category_payload_create_disable_case"
+        ]
+    )
 
 
-# def test_create_mappings(
-#     db,
-#     create_temp_workspace,
-#     add_expense_destination_attributes_1
-# ):
-#     workspace_id = 1
+def test_create_mappings(
+    db,
+    mocker,
+    create_temp_workspace,
+    add_expense_destination_attributes_1
+):
+    workspace_id = 1
 
-#     category = Category(workspace_id, "ACCOUNT", None)
+    category = Category(1, "ACCOUNT", None, sdk_connection=mocker.Mock(), destination_sync_methods=['accounts'], is_auto_sync_enabled=True, is_3d_mapping=False, charts_of_accounts=None, use_mapping_table=False)
 
-#     category.create_mappings()
+    attributes = DestinationAttribute.objects.filter(
+        workspace_id=workspace_id, attribute_type="ACCOUNT",
+        value__in=["Internet", "Meals"]
+    )
+    category.create_mappings(posted_destination_attributes=attributes)
 
-#     category_mappings = CategoryMapping.objects.filter(workspace_id=workspace_id)
+    category_mappings = CategoryMapping.objects.filter(workspace_id=workspace_id)
 
-#     assert category_mappings.count() == 2
-#     assert category_mappings[0].destination_account.value == "Internet"
-#     assert category_mappings[1].destination_account.value == "Meals"
-
-
-# def test_get_existing_fyle_attributes(
-#     db,
-#     create_temp_workspace,
-#     add_expense_destination_attributes_1,
-#     add_expense_destination_attributes_3,
-#     add_import_settings
-# ):
-#     category = Category(1, 'ACCOUNT', None)
-
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='ACCOUNT')
-#     paginated_destination_attributes_without_duplicates = category.remove_duplicate_attributes(paginated_destination_attributes)
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
-#     existing_fyle_attributes_map = category.get_existing_fyle_attributes(paginated_destination_attribute_values)
-
-#     assert existing_fyle_attributes_map == {'internet': '10091', 'meals': '10092'}
-
-#     # with code prepending
-#     category.use_code_in_naming = True
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='ACCOUNT', code__isnull=False)
-#     paginated_destination_attributes_without_duplicates = category.remove_duplicate_attributes(paginated_destination_attributes)
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
-#     existing_fyle_attributes_map = category.get_existing_fyle_attributes(paginated_destination_attribute_values)
-
-#     assert existing_fyle_attributes_map == {'123: sage300': '10095'}
+    assert category_mappings.count() == 2
+    assert category_mappings[0].destination_account.value == "Internet"
+    assert category_mappings[1].destination_account.value == "Meals"
 
 
-# def test_construct_fyle_payload_with_code(
-#     db,
-#     create_temp_workspace,
-#     add_expense_destination_attributes_1,
-#     add_expense_destination_attributes_3,
-#     add_import_settings
-# ):
-#     category = Category(1, 'ACCOUNT', None, True)
+def test_get_existing_fyle_attributes(
+    db,
+    mocker,
+    create_temp_workspace,
+    add_expense_destination_attributes_1,
+    add_expense_destination_attributes_3,
+    add_import_settings
+):
+    category = Category(1, "ACCOUNT", None, sdk_connection=mocker.Mock(), destination_sync_methods=['accounts'], is_auto_sync_enabled=True, is_3d_mapping=False, charts_of_accounts=None)
 
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='ACCOUNT')
-#     paginated_destination_attributes_without_duplicates = category.remove_duplicate_attributes(paginated_destination_attributes)
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
-#     existing_fyle_attributes_map = category.get_existing_fyle_attributes(paginated_destination_attribute_values)
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='ACCOUNT')
+    paginated_destination_attributes_without_duplicates = category.remove_duplicate_attributes(paginated_destination_attributes)
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
+    existing_fyle_attributes_map = category.get_existing_fyle_attributes(paginated_destination_attribute_values)
 
-#     # already exists
-#     fyle_payload = category.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         True
-#     )
+    assert existing_fyle_attributes_map == {'internet': '10091', 'meals': '10092'}
 
-#     assert fyle_payload == []
+    # with code prepending
+    category = Category(1, "ACCOUNT", None, sdk_connection=mocker.Mock(), destination_sync_methods=['accounts'], is_auto_sync_enabled=True, prepend_code_to_name=True, is_3d_mapping=False, charts_of_accounts=None)
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='ACCOUNT', code__isnull=False)
+    paginated_destination_attributes_without_duplicates = category.remove_duplicate_attributes(paginated_destination_attributes)
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
+    existing_fyle_attributes_map = category.get_existing_fyle_attributes(paginated_destination_attribute_values)
 
-#     # create new case
-#     existing_fyle_attributes_map = {}
-#     fyle_payload = category.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         True
-#     )
-
-#     assert fyle_payload == data["create_fyle_category_payload_with_code_create_new_case"]
+    assert existing_fyle_attributes_map == {'123: sage300': '10095'}
 
 
-# def test_disable_categories(
-#     db,
-#     mocker,
-#     create_temp_workspace,
-#     add_fyle_credentials,
-#     add_expense_destination_attributes_1,
-#     add_import_settings
-# ):
-#     workspace_id = 1
+def test_construct_fyle_payload_with_code(
+    db,
+    mocker,
+    create_temp_workspace,
+    add_expense_destination_attributes_1,
+    add_expense_destination_attributes_3,
+    add_import_settings
+):
+    category = Category(1, "ACCOUNT", None, sdk_connection=mocker.Mock(), destination_sync_methods=['accounts'], is_auto_sync_enabled=True, prepend_code_to_name=True, is_3d_mapping=False, charts_of_accounts=None)
 
-#     categories_to_disable = {
-#         'destination_id': {
-#             'value': 'old_category',
-#             'updated_value': 'new_category',
-#             'code': 'old_category_code',
-#             'updated_code': 'old_category_code'
-#         }
-#     }
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='ACCOUNT')
+    paginated_destination_attributes_without_duplicates = category.remove_duplicate_attributes(paginated_destination_attributes)
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
+    existing_fyle_attributes_map = category.get_existing_fyle_attributes(paginated_destination_attribute_values)
 
-#     ExpenseAttribute.objects.create(
-#         workspace_id=workspace_id,
-#         attribute_type='CATEGORY',
-#         display_name='Category',
-#         value='old_category',
-#         source_id='source_id',
-#         active=True
-#     )
+    # already exists
+    fyle_payload = category.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map,
+    )
 
-#     mock_platform = mocker.patch('apps.mappings.imports.modules.categories.PlatformConnector')
-#     bulk_post_call = mocker.patch.object(mock_platform.return_value.categories, 'post_bulk')
+    assert fyle_payload == []
 
-#     disable_categories(workspace_id, categories_to_disable, is_import_to_fyle_enabled=True)
+    # create new case
+    existing_fyle_attributes_map = {}
+    fyle_payload = category.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map,
+    )
 
-#     assert bulk_post_call.call_count == 1
+    assert fyle_payload == data["create_fyle_category_payload_with_code_create_new_case"]
 
-#     categories_to_disable = {
-#         'destination_id': {
-#             'value': 'old_category_2',
-#             'updated_value': 'new_category',
-#             'code': 'old_category_code',
-#             'updated_code': 'new_category_code'
-#         }
-#     }
 
-#     disable_categories(workspace_id, categories_to_disable, is_import_to_fyle_enabled=True)
-#     assert bulk_post_call.call_count == 1
+def test_disable_categories(
+    db,
+    mocker,
+    create_temp_workspace,
+    add_fyle_credentials,
+    add_expense_destination_attributes_1,
+    add_import_settings
+):
+    workspace_id = 1
 
-#     # Test disable projects with code in naming
-#     import_settings = ImportSetting.objects.get(workspace_id=workspace_id)
-#     import_settings.import_code_fields = ['ACCOUNT']
-#     import_settings.save()
+    categories_to_disable = {
+        'destination_id': {
+            'value': 'old_category',
+            'updated_value': 'new_category',
+            'code': 'old_category_code',
+            'updated_code': 'old_category_code'
+        }
+    }
 
-#     ExpenseAttribute.objects.create(
-#         workspace_id=workspace_id,
-#         attribute_type='CATEGORY',
-#         display_name='Category',
-#         value='old_category_code: old_category',
-#         source_id='source_id_123',
-#         active=True
-#     )
+    ExpenseAttribute.objects.create(
+        workspace_id=workspace_id,
+        attribute_type='CATEGORY',
+        display_name='Category',
+        value='old_category',
+        source_id='source_id',
+        active=True
+    )
 
-#     categories_to_disable = {
-#         'destination_id': {
-#             'value': 'old_category',
-#             'updated_value': 'new_category',
-#             'code': 'old_category_code',
-#             'updated_code': 'old_category_code'
-#         }
-#     }
+    mock_platform = mocker.patch('fyle_integrations_imports.modules.categories.PlatformConnector')
+    bulk_post_call = mocker.patch.object(mock_platform.return_value.categories, 'post_bulk')
 
-#     payload = [{
-#         'name': 'old_category_code: old_category',
-#         'code': 'destination_id',
-#         'is_enabled': False,
-#         'id': 'source_id_123'
-#     }]
+    disable_categories(workspace_id, categories_to_disable, is_import_to_fyle_enabled=True)
 
-#     bulk_payload = disable_categories(workspace_id, categories_to_disable, is_import_to_fyle_enabled=True)
-#     assert bulk_payload == payload
+    assert bulk_post_call.call_count == 1
+
+    categories_to_disable = {
+        'destination_id': {
+            'value': 'old_category_2',
+            'updated_value': 'new_category',
+            'code': 'old_category_code',
+            'updated_code': 'new_category_code'
+        }
+    }
+
+    disable_categories(workspace_id, categories_to_disable, is_import_to_fyle_enabled=True)
+    assert bulk_post_call.call_count == 1
+
+    # Test disable projects with code in naming
+    import_settings = ImportSetting.objects.get(workspace_id=workspace_id)
+    import_settings.import_code_fields = ['ACCOUNT']
+    import_settings.save()
+
+    ExpenseAttribute.objects.create(
+        workspace_id=workspace_id,
+        attribute_type='CATEGORY',
+        display_name='Category',
+        value='old_category_code: old_category',
+        source_id='source_id_123',
+        active=True
+    )
+
+    categories_to_disable = {
+        'destination_id': {
+            'value': 'old_category',
+            'updated_value': 'new_category',
+            'code': 'old_category_code',
+            'updated_code': 'old_category_code'
+        }
+    }
+
+    payload = [{
+        'name': 'old_category_code: old_category',
+        'code': 'destination_id',
+        'is_enabled': False,
+        'id': 'source_id_123'
+    }]
+
+    bulk_payload = disable_categories(workspace_id, categories_to_disable, is_import_to_fyle_enabled=True)
+    assert bulk_payload == payload
--- tests/test_mappings/test_imports/test_modules/test_projects.py
+++ tests/test_mappings/test_imports/test_modules/test_projects.py
@@ -1,108 +1,103 @@
-# from apps.mappings.imports.modules.projects import Project
-# from fyle_accounting_mappings.models import DestinationAttribute
-# from .fixtures import data
-# from tests.helper import dict_compare_keys
+from fyle_accounting_mappings.models import DestinationAttribute
+from fyle_integrations_imports.modules.projects import Project
+from .fixtures import data
+from tests.helper import dict_compare_keys
 
 
-# def test_construct_fyle_payload(api_client, test_connection, mocker, create_temp_workspace, add_cost_category, add_sage300_creds, add_fyle_credentials, add_project_mappings):
-#     project = Project(1, 'PROJECT', None)
+def test_construct_fyle_payload(api_client, test_connection, mocker, create_temp_workspace, add_cost_category, add_sage300_creds, add_fyle_credentials, add_project_mappings):
+    project = Project(1, 'PROJECT', None, sdk_connection=mocker.Mock(), destination_sync_methods=['jobs'], is_auto_sync_enabled=True)
 
-#     # create new case
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='PROJECT')
+    # create new case
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='PROJECT')
 
-#     existing_fyle_attributes_map = {}
-#     is_auto_sync_status_allowed = project.get_auto_sync_permission()
+    existing_fyle_attributes_map = {}
 
-#     fyle_payload = project.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         is_auto_sync_status_allowed
-#     )
+    fyle_payload = project.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map,
+    )
 
-#     assert fyle_payload == data['create_fyle_project_payload_create_new_case']
+    assert fyle_payload == data['create_fyle_project_payload_create_new_case']
 
-#     # disable case
-#     DestinationAttribute.objects.filter(
-#         workspace_id=1,
-#         attribute_type='PROJECT',
-#         value__in=['Platform APIs']
-#     ).update(active=False)
+    # disable case
+    DestinationAttribute.objects.filter(
+        workspace_id=1,
+        attribute_type='PROJECT',
+        value__in=['Platform APIs']
+    ).update(active=False)
 
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='PROJECT')
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='PROJECT')
 
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes]
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes]
 
-#     existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
+    existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
 
-#     fyle_payload = project.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         True
-#     )
+    fyle_payload = project.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map,
+    )
 
-#     assert fyle_payload == data['create_fyle_project_payload_create_disable_case']
+    assert fyle_payload == data['create_fyle_project_payload_create_disable_case']
 
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='PROJECT')
-#     paginated_destination_attributes.update(active=False)
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='PROJECT')
+    paginated_destination_attributes.update(active=False)
 
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes]
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes]
 
-#     existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
-#     existing_fyle_attributes_map['platform apis'] = '10081'
-#     existing_fyle_attributes_map['direct mail campaign'] = '10064'
+    existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
+    existing_fyle_attributes_map['platform apis'] = '10081'
+    existing_fyle_attributes_map['direct mail campaign'] = '10064'
 
-#     fyle_payload = project.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         True
-#     )
+    fyle_payload = project.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map
+    )
 
-#     assert dict_compare_keys(fyle_payload, data['create_fyle_project_payload_create_disable_case2']) == [], 'create fyle project payload create disable case2 return diffs in keys'
+    assert dict_compare_keys(fyle_payload, data['create_fyle_project_payload_create_disable_case2']) == [], 'create fyle project payload create disable case2 return diffs in keys'
 
 
-# def test_get_existing_fyle_attributes(db, create_temp_workspace, add_project_mappings, add_import_settings):
-#     project = Project(1, 'PROJECT', None)
+def test_get_existing_fyle_attributes(db, mocker, create_temp_workspace, add_project_mappings, add_import_settings):
+    project = Project(1, 'PROJECT', None, sdk_connection=mocker.Mock(), destination_sync_methods=['jobs'], is_auto_sync_enabled=True)
 
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='JOB')
-#     paginated_destination_attributes_without_duplicates = project.remove_duplicate_attributes(paginated_destination_attributes)
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
-#     existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='JOB')
+    paginated_destination_attributes_without_duplicates = project.remove_duplicate_attributes(paginated_destination_attributes)
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
+    existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
 
-#     assert existing_fyle_attributes_map == {}
+    assert existing_fyle_attributes_map == {}
 
-#     # with code prepending
-#     project.use_code_in_naming = True
-#     paginated_destination_attributes_without_duplicates = project.remove_duplicate_attributes(paginated_destination_attributes)
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
-#     existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
+    # with code prepending
+    project = Project(1, 'PROJECT', None, sdk_connection=mocker.Mock(), destination_sync_methods=['jobs'], is_auto_sync_enabled=True, prepend_code_to_name=True)
 
-#     assert existing_fyle_attributes_map == {'123: cre platform': '10065', '123: integrations cre': '10082'}
+    paginated_destination_attributes_without_duplicates = project.remove_duplicate_attributes(paginated_destination_attributes)
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
+    existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
+
+    assert existing_fyle_attributes_map == {'123: cre platform': '10065', '123: integrations cre': '10082'}
 
 
-# def test_construct_fyle_payload_with_code(db, create_temp_workspace, add_project_mappings, add_cost_category, add_import_settings):
-#     project = Project(1, 'PROJECT', None, True)
-#     project.use_code_in_naming = True
+def test_construct_fyle_payload_with_code(db, mocker, create_temp_workspace, add_project_mappings, add_cost_category, add_import_settings):
+    project = Project(1, 'PROJECT', None, sdk_connection=mocker.Mock(), destination_sync_methods=['jobs'], is_auto_sync_enabled=True, prepend_code_to_name=True)
+    project.use_code_in_naming = True
 
-#     paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='JOB')
-#     paginated_destination_attributes_without_duplicates = project.remove_duplicate_attributes(paginated_destination_attributes)
-#     paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
-#     existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
+    paginated_destination_attributes = DestinationAttribute.objects.filter(workspace_id=1, attribute_type='JOB')
+    paginated_destination_attributes_without_duplicates = project.remove_duplicate_attributes(paginated_destination_attributes)
+    paginated_destination_attribute_values = [attribute.value for attribute in paginated_destination_attributes_without_duplicates]
+    existing_fyle_attributes_map = project.get_existing_fyle_attributes(paginated_destination_attribute_values)
 
-#     # already exists
-#     fyle_payload = project.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         True
-#     )
+    # already exists
+    fyle_payload = project.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map
+    )
 
-#     assert fyle_payload == []
+    assert fyle_payload == []
 
-#     # create new case
-#     existing_fyle_attributes_map = {}
-#     fyle_payload = project.construct_fyle_payload(
-#         paginated_destination_attributes,
-#         existing_fyle_attributes_map,
-#         True
-#     )
+    # create new case
+    existing_fyle_attributes_map = {}
+    fyle_payload = project.construct_fyle_payload(
+        paginated_destination_attributes,
+        existing_fyle_attributes_map
+    )
 
-#     assert fyle_payload == data["create_fyle_project_payload_with_code_create_new_case"]
+    assert fyle_payload == data["create_fyle_project_payload_with_code_create_new_case"]
