bb41a68821ea0519d0a772d4fda2c1f106946091
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2025-07-03 21:39:34 +0530

    Feat: Fix api throttling vulnerability (#314)
    Co-authored-by: Ashutosh619-sudo <ashutosh.s@fyle.com>

--- sage_desktop_api/settings.py
+++ sage_desktop_api/settings.py
@@ -118,7 +118,15 @@ REST_FRAMEWORK = {
         'fyle_rest_auth.authentication.FyleJWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/second',
+        'user': '10/second'
+    }
 }
 
 SERVICE_NAME = os.environ.get('SERVICE_NAME')
