e171e6375abcfc699f66ea13da4f71fe78fc122a
ruuushhh <rushikeshtayade2001@gmail.com>
2025-07-03 17:10:36 +0530

    Fix test

--- tests/test_sage300/test_exceptions.py
+++ tests/test_sage300/test_exceptions.py
@@ -39,54 +39,58 @@ def test_handle_sage300_exceptions(
     add_accounting_export_expenses,
     add_accounting_export_summary,
     add_sage300_creds
-
 ):
     workspace_id = 1
     accounting_export = AccountingExport.objects.filter(workspace_id=workspace_id).first()
 
     @handle_sage300_exceptions()
-    def test_func(accounting_export, is_last_export):
+    def test_func(accounting_export_id, is_last_export):
         raise FyleCredential.DoesNotExist('Fyle credentials not found')
 
-    test_func(accounting_export, False)
+    test_func(accounting_export.id, False)
 
+    accounting_export.refresh_from_db()
     assert accounting_export.status == 'FAILED'
     assert accounting_export.detail == {'message': 'Fyle credentials do not exist in workspace'}
 
     @handle_sage300_exceptions()
-    def test_func(accounting_export, is_last_export):
+    def test_func(accounting_export_id, is_last_export):
         raise Sage300Credential.DoesNotExist('Sage300 Account not connected / token expired')
 
-    test_func(accounting_export, False)
+    test_func(accounting_export.id, False)
 
+    accounting_export.refresh_from_db()
     assert accounting_export.status == 'FAILED'
     assert accounting_export.detail == {'accounting_export_id': accounting_export.id, 'message': 'Sage300 Account not connected / token expired'}
 
     @handle_sage300_exceptions()
-    def test_func(accounting_export, is_last_export):
+    def test_func(accounting_export_id, is_last_export):
         raise WrongParamsError(response = 'Error', msg = 'Error')
 
-    test_func(accounting_export, False)
+    test_func(accounting_export.id, False)
 
     error = Error.objects.filter(workspace_id=workspace_id, accounting_export=accounting_export).first()
+    accounting_export.refresh_from_db()
     assert accounting_export.status == 'FAILED'
     assert accounting_export.detail == None
     assert error.type == 'SAGE300_ERROR'
     assert error.is_resolved == False
 
     @handle_sage300_exceptions()
-    def test_func(accounting_export, is_last_export):
+    def test_func(accounting_export_id, is_last_export):
         raise BulkError(response = 'Error', msg = 'Error')
 
-    test_func(accounting_export, False)
+    test_func(accounting_export.id, False)
 
+    accounting_export.refresh_from_db()
     assert accounting_export.status == 'FAILED'
     assert accounting_export.detail == 'Error'
 
     @handle_sage300_exceptions()
-    def test_func(accounting_export, is_last_export):
+    def test_func(accounting_export_id, is_last_export):
         raise Exception('Error')
 
-    test_func(accounting_export, False)
+    test_func(accounting_export.id, False)
 
+    accounting_export.refresh_from_db()
     assert accounting_export.status == 'FATAL'
